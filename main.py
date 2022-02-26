35b3b90c9aa0857ff7f55843ddec84aa4800b038
Jordan <38998396+TcgVanguardTroll@users.noreply.github.com>
2022-02-25 22:36:24 -0500

    Task 13: Removing duplicate functions form merge
    Task 13: This commit removes duplicates functions from the previous commit merge

--- main.py
+++ main.py
@@ -68,33 +68,3 @@ def get_user_id_from_email(email):
         return "User not found"
     else:
         return user_id[0]
-
-
-def sign_in(name, password):
-    """
-    Checks if the user is in the database and if the password is correct.
-    Returns the user's id if the user is in the database and the password is correct,
-    otherwise returns None.
-    """
-    cursor.execute("SELECT id FROM users WHERE name = %s AND password = %s", (name, password))
-    user_id = cursor.fetchone()
-    if user_id is None:
-        return "Password incorrect"
-    else:
-        return user_id[0]
-
-def sign_out(user_id):
-    """
-    Deletes the user's information from the database.
-    """
-    cursor.execute("DELETE FROM users WHERE id = %s", (user_id,))
-    db.commit()
-    print("User {} deleted".format(user_id))
-
-def get_online_users():
-    """
-    Returns a list of all the users in the database.
-    """
-    cursor.execute("SELECT name FROM users")
-    users = cursor.fetchall()
-    return users
