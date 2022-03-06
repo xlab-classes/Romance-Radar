3fc561b8f2c975458e7a7814210d3914aa9eb3f1
Alex Eastman <alexeast@buffalo.edu>
2022-03-06 14:44:16 -0500

    Added database API file with function skeletons in PHP

---
+++ db_api.php
@@ -0,0 +1,93 @@
+<!--
+Author: Alex Eastman
+Created: 03/06/2022
+Modified: 03/06/2022
+
+Database api
+-->
+<?php
+function create_db() {
+
+}
+
+function create_table() {
+
+}
+
+function destroy_db() {
+
+}
+
+function destroy_table() {
+
+}
+
+function user_exists($user_id) {
+
+}
+
+function get_user_id($email) {
+
+}
+
+function sign_in($email, $password) {
+
+}
+
+function sign_out($user_id) {
+
+}
+
+function check_password($user_id, $password) {
+
+}
+
+function update_password($user_id, $old_pwd, $new_pwd) {
+
+}
+
+function get_user_id($email) {
+
+}
+
+function create_user($name, $email, $pwd, $phone) {
+
+}
+
+function delete_user($user_id) {
+
+}
+
+function add_connection($user_id_a, $user_id_b) {
+
+}
+
+function add_connection_request($user_id_tx, $user_id_rx) {
+
+}
+
+function remove_connection($user_id_a, $user_id_b) {
+
+}
+
+function get_connections($user_id) {
+
+}
+
+function get_preferences($user_id) {
+
+}
+
+function update_preferences($user_id, $preferences) {
+
+}
+
+function get_requests($user_id) {
+
+}
+
+function get_pending($user_id) {
+    
+}
+
+?>
