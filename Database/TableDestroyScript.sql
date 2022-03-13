da7068ef5af4b9d068d12c92c462c8261771d303
Alex Eastman <alexeast@buffalo.edu>
2022-03-13 18:53:33 -0400

    Added script to destroy tables

---
+++ Database/TableDestroyScript.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS Food;
+
+DROP TABLE IF EXISTS Entertainment;
+
+DROP TABLE IF EXISTS Venue;
+
+DROP TABLE IF EXISTS Date_time;
+
+DROP TABLE IF EXISTS Date_preferences;
+
+DROP TABLE IF EXISTS Date_liked;
+
+DROP TABLE IF EXISTS Date_disliked;
+
+DROP TABLE IF EXISTS Date_ideas;
+
+DROP TABLE IF EXISTS Suggested_dates;
+
+DROP TABLE IF EXISTS Connection_requests;
+
+DROP TABLE IF EXISTS Users;
