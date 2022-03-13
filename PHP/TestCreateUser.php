138fd02e0bc396ae32f468caae381cb624afd834
Alex Eastman <alexeast@buffalo.edu>
2022-03-13 19:18:45 -0400

    Moved unit test class to new  TestCreateUser.php file and out of db_api_test.php , as required by PHPUnit #37

---
+++ PHP/TestCreateUser.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+# Import db_api.php
+require_once('db_api.php');
+use PHPUnit\Framework\TestCase;
+
+final class TestCreateUser extends TestCase
+{
+
+    public function testCreateUser(): void
+    {
+        $create_result = create_user(
+            "Jon Doe", "jon.doe@gmail.com", "password", "123 Apple Orchard Rd",
+            14541, "12/01/1980");
+        $this->assertSame(0, $create_result);
+    }
+
+}
+
+
+?>
