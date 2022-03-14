683f2d2359ea03c00d5dc697cae0b2ce8ef53cc7
Alex Eastman <alexeast@buffalo.edu>
2022-03-13 21:31:11 -0400

    Fixed issue in unit test where wrong value was being checked #37

--- PHP/TestCreateUser.php
+++ PHP/TestCreateUser.php
@@ -12,7 +12,23 @@ final class TestCreateUser extends TestCase
         $create_result = create_user(
             "Jon Doe", "jon.doe@gmail.com", "password", "123 Apple Orchard Rd",
             14541, "12/01/1980");
-        $this->assertSame(0, $create_result);
+        $this->assertSame(1, $create_result);
+
+        $result = exec_query("SELECT * FROM Users WHERE email=?", ["jon.doe@gmail.com"]);
+        $this->assertNotNull($result);
+        $this->assertNotFalse($result);
+        $this->assertSame(1, $result->num_rows);
+
+        $arr = $result->fetch_assoc();
+        $this->assertNotNull($arr);
+        $this->assertNotFalse($arr);
+        
+        $this->assertSame($arr["name"], "Jon Doe");
+        $this->assertSame($arr["email"], "jon.doe@gmail.com");
+        $this->assertSame($arr["password"], "password");
+        $this->assertSame($arr["street_address"], "123 Apple Orchard Rd");
+        $this->assertSame($arr["zipcode"], 14541);
+        $this->assertSame($arr["birthday"], "12/01/1980");
     }
 
 }
