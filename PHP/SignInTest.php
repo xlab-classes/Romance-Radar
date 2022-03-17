f265973599efa1a2ea0e6353f08f00dfdb515e92
Jordan <38998396+TcgVanguardTroll@users.noreply.github.com>
2022-03-17 11:49:13 -0400

    Changing tests to reflect hash #37

--- PHP/SignInTest.php
+++ PHP/SignInTest.php
@@ -10,7 +10,7 @@ final class SignInTest extends TestCase
     {
         # Create a test user
         create_user(
-            "Jon Doe", "doe.jon@gmail.com", "password", "123 Apple Orchard Rd",
+            "Jon Doe", "doe.jon@gmail.com", password_hash("password",PASSWORD_DEFAULT), "123 Apple Orchard Rd",
             14541, "1980/01/12");
 
         # Check that the user is in the database
@@ -45,11 +45,22 @@ final class SignInTest extends TestCase
     public function testSignInAlreadyLoggedIn(): void
     {
         # Create a test user
-        create_user("Jordan Grant","jordangrant46@yahoo.com", "#Password", "98-38 %7th Ave", "11368", "04/24/2000");
+        create_user("Jordan Grant","jordangrant46@yahoo.com", password_hash("#Password",PASSWORD_DEFAULT), "98-38 %7th Ave", "11368", "04/24/2000");
 
         # Test that the user was created successfully and is in the database
         $result = exec_query("SELECT * FROM Users WHERE email=?", ["jordangrant46@yahoo.com"]);
         $this->assertNotNull($result);
+
+        # Fetch the user's info
+        $arr = $result->fetch_assoc();
+        $this->assertNotNull($arr);
+        
+        # Check that the user's info is correct
+        $this->assertSame($arr["name"], "Jordan Grant");
+        $this->assertSame($arr["email"], "jordangrant46@yahoo.com");
+
+        # If this fails you can't sign in at all
+        $this->asserTrue(password_verify("#Password",$arr["password"]));
 
         # Sign in the dummy user
         $result = sign_in("jordangrant46@yahoo.com", "#Password");
@@ -62,7 +73,7 @@ final class SignInTest extends TestCase
 
         # Check that this second sign in attempt fails
         $this->assertEquals(0, $result);
-        echo "test passed";
+        echo "Test passed";
     }
 
     /*
@@ -74,7 +85,6 @@ final class SignInTest extends TestCase
         # Ensure there is no user with an email of "edfghijlmnop"
         $result = exec_query("SELECT * FROM Users WHERE name=?", ["abcdefghijklmnop"]);
         $this->assertEquals(0, $result->num_rows);
-        $this->assertEquals(0, sign_in("abcdefghijklmnop", "password"));
 
         # Try to sign in a user that doesn't exist
         $result = sign_in("abcd", "edfghijlmnop");
@@ -83,17 +93,27 @@ final class SignInTest extends TestCase
         $this->assertEquals(0, $result);
 
         # Create the user that doesn't exist
-        create_user("abcdefghijklmnop", "abcd", "edfghijlmnop", "b", 14214, "d");
+        create_user("abcdefghijklmnop", "abcd", password_hash("edfghijlmnop",PASSWORD_DEFAULT), "b", 14214, "d");
 
         # Test that the user was created
         $result = exec_query("SELECT * FROM Users WHERE email=?", ["abcd"]);
         $this->assertNotNull($result);
 
+        # Verify the user's info
+        $arr = $result->fetch_assoc();
+        $this->assertSame($arr["name"], "abcdefghijklmnop");
+        $this->assertSame($arr["email"], "abcd");
+        $this->assertTrue(password_verify("edfghijlmnop", $arr["password"]));
+
         # Try to sign in the user again
-        sign_in("abcd", "edfghijlmnop");
+        $sign_in_status = sign_in("abcd", "edfghijlmnop");
+
 
         # Check that this second sign in attempt succeeds
         $result = exec_query("SELECT * FROM Users WHERE email=?", ["abcd"]);
+        $this->assertNotNull($result);
+        $this->assertEquals(1, $sign_in_status); 
+
         echo "Test passed";
 
     }
