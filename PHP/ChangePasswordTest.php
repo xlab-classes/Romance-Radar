e3b739784294bfab19766a37d75d422e35dd4abe
Jordan <38998396+TcgVanguardTroll@users.noreply.github.com>
2022-03-17 22:56:41 -0400

    Fix tests change password working #37

--- PHP/ChangePasswordTest.php
+++ PHP/ChangePasswordTest.php
@@ -11,7 +11,7 @@ use PHPUnit\Framework\TestCase;
 
         public $email_ = "jdoe@meta.org";
         public $password_ = "#Password";
-        public $new_password_ = "new_password";
+        public $newpwd_ = "new_password";
 
 
     public function tearDown(): void
@@ -21,6 +21,9 @@ use PHPUnit\Framework\TestCase;
     public function testUpdatePassword(): void
     {
         $create_result = create_user("Jon Doe",$this->email_,password_hash($this->password_,PASSWORD_DEFAULT),"123",14214,"1963/12/13");
+
+        
+
         # Check that the current users password is correct
         $result = exec_query("SELECT * FROM Users WHERE email=?", [$this->email_]);
         $this->assertNotNull($result, "Result is null");
@@ -29,17 +32,24 @@ use PHPUnit\Framework\TestCase;
         $arr = $result->fetch_assoc();
         $this->assertNotNull($arr);
 
+
         # Check that the user's info is correct
         $this->assertSame($arr["name"], "Jon Doe", "Name is incorrect");
         $this->assertSame($arr["email"], $this->email_, "Email is incorrect");
         $this-> assertTrue(password_verify($this->password_, $arr["password"]), "Password is not correct");
 
-        update_password(get_user_id($this->email_),$arr["password"], password_hash($this->new_password_,PASSWORD_DEFAULT));
+        update_password(get_user_id($this->email_),$arr["password"], password_hash($this->newpwd_,PASSWORD_DEFAULT));
+
         $result = exec_query("SELECT * FROM Users WHERE email=?", [$this->email_]);
         $this->assertNotNull($result, "Result is null");
-        
-        
-        $this-> assertTrue(password_verify($this->new_password_, $arr["password"]), "Passwords are not the same");
+
+        # Fetch the user's info again
+        $arr = $result->fetch_assoc();
+        $this->assertNotNull($arr);
+
+        $verified = password_verify($this->newpwd_, $arr["password"]);
+
+        $this-> assertTrue($verified, "Password is not correct");
 
         exec_query("DELETE FROM Users WHERE email=?", [$this->email_]);
     }
@@ -61,7 +71,8 @@ use PHPUnit\Framework\TestCase;
         $this->assertSame($arr["email"], $this->email_, "Email is incorrect");
         $this-> assertTrue(password_verify($this->password_, $arr["password"]), "Password is not correct");
 
-        $this-> assertSame(0,update_password(get_user_id($this->email_),$password_, $password_));
+        $update_status = update_password(get_user_id($this->email_),$arr["password"], password_hash($this->password_,PASSWORD_DEFAULT));
+        $this->assertSame(1, $update_status, "Passwords was not updated");
 
         exec_query("DELETE FROM Users WHERE email=?", [$this->email_]);
     }
