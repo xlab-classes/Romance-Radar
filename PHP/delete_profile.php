7777408e087b538a07bceee704065ef900f3559f
Alex Eastman <alexeast@buffalo.edu>
2022-03-20 14:55:22 -0400

    Forgot to add file in last commit #55

---
+++ PHP/delete_profile.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "./db_api.php"
+
+// TODO: Clear inputs after using??
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Get user ID and make sure user is logged in
+    session_start();
+    $user_id = (int) $_SESSION["user"]["id"];
+    if (!$user_id) {
+        print("NO USER WITH THIS ID\n");
+        exit();
+    }     // User not logged in
+
+    if (delete_user($user_id)) {
+        print("Successfully deleted user\n");
+    }
+    else {
+        print("Couldn't delete user\n");
+    }
+}
+
+header("Location: ../HTML/landing.html");
