bf52e35d6101124f4203883fab17464821cad677
Alex Eastman <alexeast@buffalo.edu>
2022-03-20 11:46:54 -0400

    Added debug statement to update_profile.php #55

--- PHP/update_profile.php
+++ PHP/update_profile.php
@@ -11,6 +11,7 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') 
     start_session();
     $user_id = (int) $_SESSION["user"]["id"];
     if (!$user_id) {
+        print("NO USER WITH THIS ID\n");
         exit();
     }     // User not logged in
 
