baf55de97617166aea51b2aaaa5e6bea41371ece
Alex Eastman <alexeast@buffalo.edu>
2022-03-20 15:22:08 -0400

    Added closing tag to delete_profile.php #55

--- PHP/delete_profile.php
+++ PHP/delete_profile.php
@@ -2,7 +2,6 @@
 
 require_once "./db_api.php"
 
-// TODO: Clear inputs after using??
 if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Get user ID and make sure user is logged in
@@ -12,7 +11,7 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') 
         print("NO USER WITH THIS ID\n");
         header("Location: ../HTML/profile_page.html")
         exit();
-    }     // User not logged in
+    }
 
     if (delete_user($user_id)) {
         print("Successfully deleted user\n");
@@ -23,3 +22,5 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') 
 }
 
 header("Location: ../HTML/landing.html");
+
+?>
