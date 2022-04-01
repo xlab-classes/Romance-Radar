7aba6add34752af2c9f4b8d5549ff851cd79313a
jordangrant <jordangrant46@yahoo.com>
2022-03-31 21:54:44 -0400

    #7  Create a function that generates capcha

---
+++ PHP/verification.php
@@ -0,0 +1,16 @@
+<?php
+
+// File that contains account verification logic via a captcha
+
+/* This function generates a random string of a random length */
+function generate_captcha(){
+    $length = rand(5, 10);
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
