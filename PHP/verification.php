9cd35207c4144a2056046a8dd45c8f664e4dabaa
jordangrant <jordangrant46@yahoo.com>
2022-03-31 22:07:40 -0400

    #7 Working on functions frontend will call

--- PHP/verification.php
+++ PHP/verification.php
@@ -14,3 +14,32 @@ function generate_captcha(){
     return $randomString;
 }
 
+/* Comapres user input to the genrated capcha */
+function compare_captcha($captcha, $user_input){
+    if($captcha == $user_input){
+        return true;
+    }
+    return false;
+}
+
+/* Verifies that the user is a valid user via a captcha */
+function verify_user($id, $captcha){
+
+    // Get user-inputted captcha from the front-end testbox
+
+    if(empty($id) || empty($captcha)){
+        return false;
+    }
+    
+    //TODO(Jordan): Get user-inputted captcha from the front-end textbox
+    $user_input = $_POST['captcha'];
+
+    $user_status = user_exists($id);
+    if($user_status == false){
+        return false;
+    }
+    if(compare_captcha($captcha, $user_input)){
+        return true;
+    }
+    return false;
+}
