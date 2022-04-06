4b06e741ab5a5c365e88af814ddf7246ccd46f64
TcgVanguardTroll <jordangrant46@gmail.com>
2022-04-06 19:53:03 -0400

    #94 working more on function headers

--- PHP/privacy_settings.php
+++ PHP/privacy_settings.php
@@ -1,6 +1,20 @@
 <?php
 
+/* This file contains the main api regarding the privacy settings functionality in our front-end */
 require_once 'db_api.php';
 
 session_start();
 
+function hide_all_privacy_settings($id) {
+    $user_exists = user_exists($id);
+    if ($user_exists == false) {
+        return 0;
+    }
+}
+
+function show_all_privacy_settings($id) {
+    $user_exists = user_exists($id);
+    if ($user_exists == false) {
+        return 0;
+    }
+}
