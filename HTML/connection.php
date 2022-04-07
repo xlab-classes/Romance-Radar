885cd2e41992503736ed6a8d8e2beeddd08cb3a9
Swastik Naik <swasnaiktik@gmail.com>
2022-04-07 19:09:35 -0400

    Connected structure complete with icons #88

--- HTML/connection.php
+++ HTML/connection.php
@@ -57,6 +57,21 @@ if(!is_null($user['partner'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        body{
+            background-color: #FFC0CB;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            
+        }
+        .rounded-input{
+            border-radius: 25px 0 0 25px;
+        }
+        .rounded-submit{
+            border-radius: 0 25px 25px 0px;
+            background-color: #FF4F4F;
+        }
+        
+    </style>
 </head>
 <body>
     <div class="container-fluid">
