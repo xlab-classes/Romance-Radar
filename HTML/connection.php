f3f64965926ecd740c47d6b164ace0e43057fcb7
Swastik Naik <swasnaiktik@gmail.com>
2022-04-07 16:38:30 -0400

    connection without match reconfigured #88

--- HTML/connection.php
+++ HTML/connection.php
@@ -1,36 +1,68 @@
-
 <?php
+require_once '../PHP/db_api.php';
+require_once '../PHP/helper.php';
 
-require '../PHP/db_api.php';
-require '../PHP/helper.php';
+session_start();
+if(!isset($_SESSION['user'])){
+    echo 'Please Login!!';
+    header('./login.html');
+    exit();
+}
 
-$user = getUser("2","test2")->fetch_assoc();
-// echo $user->fetch_assoc()['email'];
+$user = $_SESSION['user'];
+
+if(!is_null($user['partner'])){
+    $display = sprintf('
+    <div class="row pt-5">
+            <div class="col text-center"><h3>What are you waiting for?</h3></div>
+        </div>
+        <div class="row pt-5 g-5">
+            <div class="col-4">
+                <img src="../assets/cupid.png" class="img-fluid">
+            </div>
+
+            <div class="col">
+                <div class="row">
+                    <p class="col fst-italic fw-light">
+                        Let\'s face it, coming up with a date idea that\'s as fun and unique as your relationship can be just as hard as finding someone to date in the first place. Whether you\'re commuting to work or traveling 20 steps from your bed to your desk and back again, most of us just don\'t have a lot of creative juices left over when we\'re done for the day. 
+                        <br/><br/>Leave the creativity to us!
+                        <br/><br/><br/>Match with your soulmate right now!
+                    </p>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control rounded-input opacity-75">
+                            <input value="submit" type="button" class="input-group-text rounded-submit"></input>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="row"><div class="col"><img src="%s" class="img-fluid rounded-circle"></div></div>
+                <div class="row"><div class="col text-center">%s</div></div>
+            </div>
+        </div>
+    ', $user['user_picture'], $user['name']);
+}else{
+
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../CSS/connection.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>connection Page</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <h1><?= $user['name']?></h1>
-    <h2>“When I need a pick me up, I just think of your laugh and it makes me smile”</h2>
-    <h3> &#128222;</h3>
-    <h4>+17165555555</h4>
-    <h5>&#128231;</h5>
-    <h6><?= $user['email']?></h6>
-    <p>&#127969; </p>
-    <p class="address"><?= $user['street_address']?></p>
-    <p class="distance1">
-        &#128205;</p>
-    <p class="distance2">
-        10km</p>
-    <p class="money1"> &#128176; </p>
-    <p class="money2">$500</p>
-    <p class="date">Date Ideas</p>
-    <img src="<?= $user['user_picture']?>" alt="Couple image" allign="right">
+    <div class="container-fluid">
+        <?php
+        echo $display;
+        ?>
+    </div>
 </body>
 </html>
