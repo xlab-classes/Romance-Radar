7d06ce0ab2b3ddfc9ea49df3cad66b74186ebf0e
Swastik Naik <swasnaiktik@gmail.com>
2022-04-08 21:29:35 -0400

    Connection request being sent successfully #88

--- PHP/modify_connections.php
+++ PHP/modify_connections.php
@@ -3,7 +3,21 @@
 require './db_api.php';
 require './helper.php';
 #Checking request method
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['remove_connection'])) {
-    print_r($_POST);
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['connection_request'])) {
+    $email = $_POST['connection_request'];
+    $result = getUser(NULL, $email);
+    if(!$result){
+        echo 'Email malformed';
+        exit();
+    }
+    if(!$other_user = $result->fetch_assoc()){
+        echo 'User not found';
+        exit();
+    }
+    print_r($other_user);
+    if(!add_connection_request((int)$_SESSION['user']['id'], (int)$other_user['id'])){
+        echo 'Adding connection request failed';
+    }
+    header('Location: ../HTML/connection.php');
 }
 ?>
