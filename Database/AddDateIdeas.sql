299624fe7d0ababb71be0d34723d43ef4a40032b
Alex Eastman <alexeast@buffalo.edu>
2022-04-09 13:46:46 -0400

    Attempt typo fix #92

--- Database/AddDateIdeas.sql
+++ Database/AddDateIdeas.sql
@@ -2,4 +2,4 @@ INSERT INTO Date_ideas
 (name, description, picture, time, location) 
 VALUES
 ("Coffee", "Share a chat over some warm joe", "../assets/coffee.jpg",
-    "20220601 12:00:00 PM", "123 Tim Hortons Lane");
+    "2022-06-01 12:00:00 PM", "123 Tim Hortons Lane");
