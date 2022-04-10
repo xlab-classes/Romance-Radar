81ec40c1518bf64d8472ff9ec58e34596172b736
Alex Eastman <alexeast@buffalo.edu>
2022-04-10 15:05:54 -0400

    Updated reset_Database script #92

--- PHP/tests/reset_database.sh
+++ PHP/tests/reset_database.sh
@@ -2,14 +2,14 @@
 
 echo "Truncating tables...\n"
 sleep 1
-mysql -h "oceanus" -u "alexeast" -p "50252636" "cse442_2022_spring_team_j_db" < "../../Database/TableDestroyScript.sql"
+mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/TableDestroyScript.sql"
 
 echo "Creating tables...\n"
 sleep 1
-mysql -h "oceanus" -u "alexeast" -p "50252636" "cse442_2022_spring_team_j_db" < "../../Database/TableCreateScript.sql"
+mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/TableCreateScript.sql"
 
 echo "Adding date ideas...\n"
 sleep 1
-mysql -h "oceanus" -u "alexeast" -p "50252636" "cse442_2022_spring_team_j_db" < "../../Database/AddDateIdeas.sql"
+mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/AddDateIdeas.sql"
 
 echo "Done!\n"
