a7753b1c7a8cca3697a386ff7156dae65b0507a0
Alex Eastman <alexeast@buffalo.edu>
2022-04-10 15:09:37 -0400

    Test #92

--- PHP/tests/reset_database.sh
+++ PHP/tests/reset_database.sh
@@ -2,7 +2,7 @@
 
 echo "Truncating tables...\n"
 sleep 1
-mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/TableDestroyScript.sql" | grep -v "[Warning] Using a password"
+mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/TableDestroyScript.sql" 2>&1 | grep -v "[Warning] Using a password"
 
 echo "Creating tables...\n"
 sleep 1
