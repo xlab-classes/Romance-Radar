f3e145ea8a29d517062998317a5fd8ecfb5714e0
Alex Eastman <alexeast@buffalo.edu>
2022-04-10 15:19:11 -0400

    Removed newlines from echos in reset_database script #92

--- PHP/tests/reset_database.sh
+++ PHP/tests/reset_database.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "Truncating tables...\n"
+echo "Truncating tables..."
 sleep 1
 mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/TableDestroyScript.sql"
 
-echo "Creating tables...\n"
+echo "Creating tables..."
 sleep 1
 mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/TableCreateScript.sql"
 
-echo "Adding date ideas...\n"
+echo "Adding date ideas..."
 sleep 1
 mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/AddDateIdeas.sql"
 
-echo "Done!\n"
+echo "Done!"
