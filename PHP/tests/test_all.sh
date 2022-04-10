bfe6570a1123976a375e685cd80992dc5dc8d204
Alex Eastman <alexeast@buffalo.edu>
2022-04-10 15:26:26 -0400

    Added better echos in test_all scirpt #92

--- PHP/tests/test_all.sh
+++ PHP/tests/test_all.sh
@@ -6,39 +6,51 @@ if [[ $option == "y" || $option == "Y" ]]; then
     ./reset_database.sh
 fi
 
-echo "Testing create_user"
+echo -e "\n*** TESTING CREATE USER ***\n"
 ./phpunit TestCreateUser.php
+echo "****************************************"
 
-echo "Testing sign_in"
+
+echo -e "\n*** TESTING SIGN IN ***\n"
 ./phpunit TestSignIn.php
+echo "****************************************"
 
-echo "Testing update_preferences"
+echo -e "\n*** TESTING UPDATE PREFERENCES ***\n"
 ./phpunit TestUpdatePreferences.php
+echo "****************************************"
 
-echo "Testing updating personal details"
+echo -e "\n*** TESTING UPDATE PERSON DETAILS ***\n"
 ./phpunit TestUpdatePersonalDetails.php
+echo "****************************************"
 
-echo "Testing update_password"
+echo -e "\n*** TESTING UPDATE PASSWORD ***\n"
 ./phpunit TestChangePassword.php
+echo "****************************************"
 
-echo "Testing connection functions:"
-echo "\tadd_connection_request"
-echo "\tadd_connection"
-echo "\tremove_connection_request"
-echo "\tremove_connection"
+echo -e "\n*** TESTING CONNECTION FUNCTIONS ***\n"
+echo -e "\tadd_connection_request"
+echo -e "\tadd_connection"
+echo -e "\tremove_connection_request"
+echo -e "\tremove_connection"
 ./phpunit TestConnections.php
+echo "****************************************"
 
-echo "Testing get_date_id"
+echo -e "\n*** TESTING GET DATE ID ***\n"
 ./phpunit TestGetDateId.php
+echo "****************************************"
 
-echo "Testing get_date_ids"
+echo -e "\n*** TESTING GET DATE IDS ***\n"
 ./phpunit TestGetDateIds.php
+echo "****************************************"
 
-echo "Testing get_date_information"
+echo -e "\n*** TESTING GET DATE INFORMATION ***\m"
 ./phpunit TestGetDateInformation.php
+echo "****************************************"
 
-echo "Testing add_tag"
+echo -e "\n*** TESTING ADD TAG ***\n"
 ./phpunit TestAddTag.php
+echo "****************************************"
 
-echo "Testing generate_dates"
+echo -e "\n*** TESTING GENERATE DATES ***\n"
 ./phpunit TestGenerateDates.php
+echo "****************************************"
