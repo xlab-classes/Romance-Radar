dc0efeb78a5a7994f22cb5c9a9a7dbc22092bcca
Alex Eastman <alexeast@buffalo.edu>
2022-04-10 13:01:48 -0400

    Added bash script to reset database to clean state with date ideas #92

---
+++ PHP/tests/reset_database.sh
@@ -0,0 +1,11 @@
+echo "Truncating tables...\n"
+sleep 1
+# TableDestroyScript.sql
+
+echo "Creating tables...\n"
+sleep 1
+# TableCreateScript.sql
+
+echo "Adding date ideas...\n"
+sleep 1
+# AddDateIdeas.sql
