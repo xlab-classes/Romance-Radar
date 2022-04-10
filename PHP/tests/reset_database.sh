550150ca89d6aaaa870278f99e19508d84bb3b50
Alex Eastman <alexeast@buffalo.edu>
2022-04-10 15:09:02 -0400

    Suppressing password warning in reset_database script #92

--- PHP/tests/reset_database.sh
+++ PHP/tests/reset_database.sh
@@ -2,14 +2,14 @@
 
 echo "Truncating tables...\n"
 sleep 1
-mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/TableDestroyScript.sql"
+mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/TableDestroyScript.sql" | grep -v "[Warning] Using a password"
 
 echo "Creating tables...\n"
 sleep 1
-mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/TableCreateScript.sql"
+mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/TableCreateScript.sql" | grep -v "[Warning] Using a password"
 
 echo "Adding date ideas...\n"
 sleep 1
-mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/AddDateIdeas.sql"
+mysql -h "oceanus" -u "alexeast" --password="50252636" -D "cse442_2022_spring_team_j_db" < "../../Database/AddDateIdeas.sql" | grep -v "[Warning] Using a password"
 
 echo "Done!\n"
