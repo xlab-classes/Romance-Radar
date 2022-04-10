8d6d6fe32fed0a161efa6f3f997cf1985e663801
Alex Eastman <alexeast@buffalo.edu>
2022-04-10 15:03:31 -0400

    Added interpreter to shell scripts #92

--- PHP/tests/reset_database.sh
+++ PHP/tests/reset_database.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Truncating tables...\n"
 sleep 1
 mysql -h "oceanus" -u "alexeast" -p "50252636" "cse442_2022_spring_team_j_db" < "../../Database/TableDestroyScript.sql"
--- PHP/tests/test_all.sh
+++ PHP/tests/test_all.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "OK to clear database for testing? y/N\n"
 read option
 
