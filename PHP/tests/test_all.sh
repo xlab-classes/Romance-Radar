b5fbd883d7f9de71b6746cbefbeafaf76289cb65
Alex Eastman <alexeast@buffalo.edu>
2022-04-10 12:41:18 -0400

    Created bash script to run all unit tests #92

---
+++ PHP/tests/test_all.sh
@@ -0,0 +1,33 @@
+echo "Testing create_user\n"
+./phpunit TestCreateUser.php
+
+echo "Testing sign_in\n"
+./phpunit TestSignIn.php
+
+echo "Testing update_preferences\n"
+./phpunit TestUpdatePreferences.php
+
+echo "Testing updating personal details\n"
+./phpunit TestUpdatePersonalDetails.php
+
+echo "Testing update_password\n"
+./phpunit TestChangePassword.php
+
+echo "Testing connection functions:\n"
+echo "\tadd_connection_request\n"
+echo "\tadd_connection\n"
+echo "\tremove_connection_request\n"
+echo "\tremove_connection\n"
+./phpunit TestConnections.php
+
+echo "Testing get_date_ids\n"
+./phpunit TestGetDateIds.php
+
+echo "Testing get_date_information\n"
+./phpunit TestGetDateInformation.php
+
+echo "Testing add_tag\n"
+./phpunit TestAddTag.php
+
+echo "Testing generate_dates\n"
+./phpunit TestGenerateDates.php
