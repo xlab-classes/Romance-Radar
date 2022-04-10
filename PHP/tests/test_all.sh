5802a7fa67f855179119653dd15fb78465180b04
Alex Eastman <alexeast@buffalo.edu>
2022-04-10 15:18:49 -0400

    Removed unneeded newline characters from test_all script. Removed exit #92

--- PHP/tests/test_all.sh
+++ PHP/tests/test_all.sh
@@ -3,45 +3,43 @@
 read -p "OK to clear database for testing? Y/N: " option
 
 if [[ $option == "y" || $option == "Y" ]]; then
-    echo "Clearing database...\n"
+    echo "Clearing database..."
     ./reset_database.sh
 fi
 
-exit
-
-echo "Testing create_user\n"
+echo "Testing create_user"
 ./phpunit TestCreateUser.php
 
-echo "Testing sign_in\n"
+echo "Testing sign_in"
 ./phpunit TestSignIn.php
 
-echo "Testing update_preferences\n"
+echo "Testing update_preferences"
 ./phpunit TestUpdatePreferences.php
 
-echo "Testing updating personal details\n"
+echo "Testing updating personal details"
 ./phpunit TestUpdatePersonalDetails.php
 
-echo "Testing update_password\n"
+echo "Testing update_password"
 ./phpunit TestChangePassword.php
 
-echo "Testing connection functions:\n"
-echo "\tadd_connection_request\n"
-echo "\tadd_connection\n"
-echo "\tremove_connection_request\n"
-echo "\tremove_connection\n"
+echo "Testing connection functions:"
+echo "\tadd_connection_request"
+echo "\tadd_connection"
+echo "\tremove_connection_request"
+echo "\tremove_connection"
 ./phpunit TestConnections.php
 
-echo "Testing get_date_id\n"
+echo "Testing get_date_id"
 ./phpunit TestGetDateId.php
 
-echo "Testing get_date_ids\n"
+echo "Testing get_date_ids"
 ./phpunit TestGetDateIds.php
 
-echo "Testing get_date_information\n"
+echo "Testing get_date_information"
 ./phpunit TestGetDateInformation.php
 
-echo "Testing add_tag\n"
+echo "Testing add_tag"
 ./phpunit TestAddTag.php
 
-echo "Testing generate_dates\n"
+echo "Testing generate_dates"
 ./phpunit TestGenerateDates.php
