b1ae8b25b189e703cc538c0cfacfda1287c53725
Alex Eastman <alexeast@buffalo.edu>
2022-04-11 17:14:37 -0400

    added assertion to test get preferences #92

--- PHP/tests/TestGetPreferences.php
+++ PHP/tests/TestGetPreferences.php
@@ -66,15 +66,8 @@ final class TestGetPreferences extends TestCase
         );
 
         $this->assertEquals(1, update_preferences($this->id, $prefs));
-        // $this->assertSame($prefs, get_preferences($this->id));
-
         $setprefs = get_preferences($this->id);
-        foreach ($setprefs as $k => $v) {
-            print("Category: $k\n");
-            foreach ($v as $kv => $vv) {
-                print("Key: $kv, Value: $vv\n");
-            }
-        }
+        $this->assertSame($prefs, get_preferences($this->id));
     }
 
 }
