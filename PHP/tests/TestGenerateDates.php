9575082464cc83783f7dceb89276ef336a537277
Alex Eastman <alexeast@buffalo.edu>
2022-04-11 16:34:35 -0400

    Removed erroneous character #92

--- PHP/tests/TestGenerateDates.php
+++ PHP/tests/TestGenerateDates.php
@@ -73,7 +73,7 @@ final class TestGenerateDates extends TestCase
         $this->assertGreaterThan(0, $this->id_b, "Error getting ID of user B in setUp() function");
         $this->assertGreaterThan(0, $this->id_c, "Error getting ID of user C in setUp() function");
         $this->assertGreaterThan(0, $this->id_d, "Error getting ID of user D in setUp() function");
-s
+
         $this->assertNotEquals($this->id_a, $this->id_b);
         $this->assertNotEquals($this->id_a, $this->id_c);
         $this->assertNotEquals($this->id_a, $this->id_d);
