8dce84627a366a770eb2aa7193ace519b9e8a3da
Swastik Naik <swasnaiktik@gmail.com>
2022-04-13 17:38:20 -0400

    css added to the simple chat #81

--- HTML/chat.php
+++ HTML/chat.php
@@ -44,36 +44,38 @@ foreach($messages as $msg){
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
-      body{
+    body{
             background-color: #FFC0CB;
             font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
             color: #9F1111;
         }
 
-        .text-left {
-  text-align: left;
-}
+    .text-left {
+        text-align: left;
+        }
 
-.text-right {
-  text-align: right;
-}
+    .text-right {
+        text-align: right;
+        }
 
-.text-center {
-  text-align: center;
-}
+    .text-center {
+        text-align: center;
+        }
 
+    .iframe{
+        height: 50vh;
+        width: 100%;
+    }
 
-
-.mask-custom {
-  background: rgba(24, 24, 16, .2);
-  border-radius: 2em;
-  backdrop-filter: blur(15px);
-  border: 2px solid rgba(255, 255, 255, 0.05);
-  background-clip: padding-box;
-  box-shadow: 10px 10px 10px rgba(46, 54, 68, 0.03);
-}
-
-    </style>
+    .mask-custom {
+        background: rgba(24, 24, 16, .2);
+        border-radius: 2em;
+        backdrop-filter: blur(15px);
+        border: 2px solid rgba(255, 255, 255, 0.05);
+        background-clip: padding-box;
+        box-shadow: 10px 10px 10px rgba(46, 54, 68, 0.03);
+        }
+  </style>
 
   </head>
   <body>
@@ -81,103 +83,29 @@ foreach($messages as $msg){
   <section class="chat">
   <div class="container py-5">
     <div class="row justify-content-center">
-
-
       <div class="col-md-6 col-lg-7 col-xl-7">
         <ul class="list-unstyled text-white">
-          <li class="d-flex justify-content-between mb-4">
-            <?php
-                            echo '<img id="profile_picture" src="'.$_SESSION['user']['user_picture'].'"
-              alt="avatar"
-              class="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
-              width="60" >'
-              ?>
-
-            <div class="card mask-custom w-100">
-              <div
-                class="card-header d-flex justify-content-between p-3"
-                style="border-bottom: 1px solid rgba(255, 255, 255, 0.3);"
-              >
-                <p class="fw-bold mb-0"><?php echo $_SESSION['user']['name']; ?></p>
-                <p class="text-light small mb-0">
-                  <i class="far fa-clock"></i> 13 mins ago
-                </p>
-              </div>
-              <div class="card-body">
-                <p class="mb-0">
-                  Hi Lauren! How you doing?
-                </p>
-              </div>
-            </div>
-
-          </li>
-          <li class="d-flex justify-content-between mb-4">
-            <?php
-                            echo '<img id="profile_picture" src="'.$_SESSION['user']['user_picture'].'"
-              alt="avatar"
-              class="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
-              width="60" >'
-              ?>
-
-            <div class="card mask-custom w-100">
-              <div
-                class="card-header d-flex justify-content-between p-3"
-                style="border-bottom: 1px solid rgba(255, 255, 255, 0.3);"
-              >
-                <p class="fw-bold mb-0"><?php echo $_SESSION['partner']['name']; ?></p>
-                <p class="text-light small mb-0">
-                  <i class="far fa-clock"></i> 10 mins ago
-                </p>
-              </div>
-              <div class="card-body">
-                <p class="mb-0">
-                  Hi Brad! Doing great! How about you?
-                </p>
-              </div>
-            </div>
-
-          </li>
-      <!--    <li class="d-flex justify-content-between mb-4">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
-              alt="avatar"
-              class="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
-              width="60"
-            />
-            <div class="card mask-custom w-100">
-              <div
-                class="card-header d-flex justify-content-between p-3"
-                style="border-bottom: 1px solid rgba(255, 255, 255, 0.3);"
-              >
-                <p class="fw-bold mb-0">Brad</p>
-                <p class="text-light small mb-0">
-                  <i class="far fa-clock"></i> 8 mins ago
-                </p>
-              </div>
-              <div class="card-body">
-                <p class="mb-0">
-                  Great! Thanks for asking. It's been a hetric week. Can't wait for the weekend. What's your plan for the weekend?
-                </p>
-              </div>
-            </div>
-
-          </li> -->
+            <li class="d-flex justify-content-between mb-4">
+            <iframe src="./messages.php" frameborder="0" class="iframe"></iframe>
+            </li>
 
           <li class="mb-3">
-            <div class="form-outline form-white">
-              <textarea class="form-control" id="textAreaExample" rows="4"></textarea>
-              <label class="form-label" for="textAreaExample">Message</label>
-            </div>
-          </li>
-          <button type="button" class="btn btn-light btn-lg btn-rounded float-end">
-            Send
-          </button>
+          <form action="./chat.php" method="post" enctype="multipart/form-data">
+                <div class="form-outline form-white">
+                <textarea class="form-control" id="textAreaExample" rows="4" name="message" required></textarea>
+                </div>
+            </li>
+            <button type="Submit" class="btn btn-light btn-lg btn-rounded float-end">
+                Send
+            </button>
+           </form>
         </ul>
       </div>
     </div>
   </div>
 </section>
+  
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
+
+</body>
 </html>
