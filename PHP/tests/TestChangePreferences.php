168d8b732a51ecfe96286492aa0695a5b617a533
jordangrant <jordangrant46@yahoo.com>
2022-04-16 12:46:18 -0400

    #111 Wrapping up tests

--- PHP/tests/TestChangePreferences.php
+++ PHP/tests/TestChangePreferences.php
@@ -79,5 +79,80 @@ final class TestChangePreferences extends TestCase
         this->assertEquals(1,$prefs["Food"]["cafe"],"Cafe Should be 1");
         this->assertEquals(1,$prefs["Food"]["restaurant"],"Restaurant should be 1");
         }
+    
+        function testEntertainmentPreferencesChanged(){
+
+            // Create an array conatinng concerts and bar
+            $entertainment_prefs = array("concerts", "bar");
+
+            // Change the entertainment preferences
+            change_entertainment_preferences($this->id,$this->prefs, $entertainment_prefs);
+
+            // Get the value of the entertainment preferences
+            $prefs = get_preferences($this->id);
+
+            // Check that concerts and bar are now 0
+            $this->assertEquals(0, $prefs["Entertainment"]["concerts"], "concerts is not 0");
+            $this->assertEquals(0, $prefs["Entertainment"]["bar"], "bar should be 0");
+
+            // Check that the other preferences haven't chnaged
+            this->assertEquals(1,$prefs["Entertainment"]["hiking"],"hiking should be 1");
+        }
+
+        function testVenuePreferencesChanged(){
+
+            // Create an array conatinng indoors and social_events
+            $venue_prefs = array("indoors", "social_events");
+
+            // Change the venue preferences
+            change_venue_preferences($this->id,$this->prefs, $venue_prefs);
+
+            // Get the value of the venue preferences
+            $prefs = get_preferences($this->id);
+
+            // Check that indoors and social_events are now 0
+            $this->assertEquals(0, $prefs["Venue"]["indoors"], "indoors is not 0");
+            $this->assertEquals(0, $prefs["Venue"]["social_events"], "social_events should be 0");
+
+            // Check that the other preferences haven't chnaged
+            this->assertEquals(1,$prefs["Venue"]["outdoors"],"outdoors should be 1");
+        }
+
+        function testDateTimeChanged(){
+                
+                // Create an array conatinng morning and evening
+                $date_time_prefs = array("morning", "evening");
+    
+                // Change the date_time preferences
+                change_date_time_preferences($this->id,$this->prefs, $date_time_prefs);
+    
+                // Get the value of the date_time preferences
+                $prefs = get_preferences($this->id);
+    
+                // Check that morning and evening are now 0
+                $this->assertEquals(0, $prefs["Date_time"]["morning"], "morning is not 0");
+                $this->assertEquals(0, $prefs["Date_time"]["evening"], "evening should be 0");
+    
+                // Check that the other preferences haven't chnaged
+                this->assertEquals(1,$prefs["Date_time"]["afternoon"],"afternoon should be 1");
+        }
+
+        function testChangeDateCost(){
+
+            // Date Cost Ranges.
+            $cheap = 1;
+            $medium = 500;
+            $expesnive = 10000;
+
+            change_date_cost($this->id, $cheap);
+            $prefs = get_preferences($this->id);
+            $this->assertEquals($cheap, $prefs["Date_preferences"]["cost"], "cost should be $cheap");
+            change_date_cost($this->id, $medium);
+            $prefs = get_preferences($this->id);
+            $this->assertEquals($medium, $prefs["Date_preferences"]["cost"], "cost should be $medium");
+            change_date_cost($this->id, $expesnive);
+            $prefs = get_preferences($this->id);
+            $this->assertEquals($expesnive, $prefs["Date_preferences"]["cost"], "cost should be $expesnive");
+        }
 
 }
