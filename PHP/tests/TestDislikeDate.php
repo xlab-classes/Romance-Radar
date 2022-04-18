3d63980d95b48b3244b03b0032107664a5d54a2a
Alex Eastman <alexeast@buffalo.edu>
2022-04-17 21:22:55 -0400

    Added basic test to TestDisikeDate #112

--- PHP/tests/TestDislikeDate.php
+++ PHP/tests/TestDislikeDate.php
@@ -35,4 +35,17 @@ final class TestDislikeDate extends TestCase
         delete_user($this->id);
     }
 
+    function testBasic(): void
+    {
+        $this->assertEquals(1, dislike_date($this->id, 1), "Couldn't dislike date");
+        $this->assertEquals(1, dislike_date($this->id, 1), "Couldn't re-dislike date");
+        
+        $query = "SELECT * FROM Dates_disliked WHERE id=? AND date_id=?";
+        $data = [$this->id, 1];
+        $result = exec_query($query, $data);
+
+        $this->assertNotNull($result, "Couldn't exec_query");
+        $this->assertEquals(1, $result->num_rows, "No entries in dates_disliked");
+    }
+
 }
