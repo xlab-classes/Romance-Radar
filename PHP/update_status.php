307d7a5efd3b435593c18ac6110d09c452b94693
Alex Eastman <alexeast@buffalo.edu>
2022-04-23 19:45:56 -0400

    Back to debugging update_status script #129

--- PHP/update_status.php
+++ PHP/update_status.php
@@ -15,8 +15,10 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'GET') {
 
     if (isset($_GET['status'])) {
         set_status($user_id, $_GET['status']);
+        $status = get_status($user_id);
+        echo "$status";
     }
 
 }
 
-header("Location: ../HTML/profile_page.php");
+// header("Location: ../HTML/profile_page.php");
